3c0e2651c9d3bd17898337d4eddfcd7d94c1d2a2
koorochka <artemij@koorochka.ru>
2019-06-17 23:04:28 +0300

    Powered by Artem Koorochka

---
+++ bitrix/php_interface/this_site_support.php
@@ -0,0 +1,3 @@
+Сайт разработан в агентстве <a href="http://ysgroup.ru/">YSGroup</a>
+<br>
+Поддержка:<a href="http://koorochka.ru">&copy; <span style="color:#993300;">ak</span>soft-studio <?=date("d.m.Y")?></a>
