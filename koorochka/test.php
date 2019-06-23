5f09615c688218858aa8a33646f727a8e6a1a6e3
koorochka <artemij@koorochka.ru>
2019-06-23 20:59:06 +0300

    coffee and machine

--- koorochka/test.php
+++ koorochka/test.php
@@ -1,14 +1,36 @@
 <?
 /**
  * Test Request
+ * @var Cmain $APPLICATION
  */
 use Bitrix\Main\Application;
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $request = Application::getInstance();
+
+
+
+
 $request = $request->getContext();
+
+d($request->getServer()->getHttpHost()); // dev.vpcoffee.ru
+d($request->getServer()->getRequestMethod()); // POST
+d($request->getServer()->getServerAddr()); // 127.0.0.1
+d($request->getServer()->getServerName()); // dev.vpcoffee.ru
+
+
 $request = $request->getRequest();
 
-d($_REQUEST);
+d($request->getHttpHost()); // dev.vpcoffee.ru
+d($request->getRequestUri()); // /koorochka/test.php
+d($request->getPost("TEST")); // testing
+while($param = $request->getPostList()->next())
+{
+    d($param);
+}
+d($request->toArray());
+
+d(($request->isHttps() ? "https://" : "http://") . $request->getHttpHost() . SITE_DIR . "order/order_form.php");
+
 
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
