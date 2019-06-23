daed4665f38cf8fab0f0dd6b568541d57391f6b2
koorochka <artemij@koorochka.ru>
2019-06-23 20:25:25 +0300

    work with full adress

--- koorochka/http.lient.php
+++ koorochka/http.lient.php
@@ -4,14 +4,17 @@
  * 23.06.2019 19:51
  * https://mrcappuccino.ru/blog/post/work-with-http-bitrix-d7
  * https://www.intervolga.ru/blog/projects/d7-analogi-lyubimykh-funktsiy-v-1s-bitriks/
+ * https://dev.1c-bitrix.ru/api_d7/bitrix/main/web/httpclient/__construct.php
  */
 use Bitrix\Main\Web\HttpClient;
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $arParams = array(
-    "URL" => "/koorochka/test.php",
-    "URL_TEST" => "/koorochka/test.php"
+    "URL" => "http://dev.vpcoffee.ru/koorochka/test.php",
+    "URL_TEST" => "/koorochka/test.php",
+    "METHOD" => HttpClient::HTTP_POST
 );
+
 
 $arResult = array(
     "TEST" => "Test HttpClient",
@@ -20,8 +23,7 @@ $arResult = array(
     "TYPE" => null,
 );
 
-$httpClient = new HttpClient();
-//$httpClient->query($method, $url, $entityBody = null);
+$httpClient = new HttpClient(array("version" => "1.1", "socketTimeout" => 30, "streamTimeout" => 30, "redirect" => true, "redirectMax" => 5));
 
 //$httpClient->setRedirect(true, 5);
 $httpClient->setHeader("Content-Type","application/octet-stream");
